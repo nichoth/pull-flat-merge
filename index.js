7fef60de47eb867e8a57962e4984ef061cc264e7
Nick Thomas <nichoth@gmail.com>
2016-10-17 09:35:18 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var S = require('pull-stream/pull')
+S.drain = require('pull-stream/sinks/drain')
+var many = require('pull-many')
+
+module.exports = function flatMerge () {
+    return function sink (source) {
+        var m = many()
+        S(source, S.drain(function onEvent (s) {
+            m.add(s)
+        }, function onEnd (err) {
+            // m.cap()
+        }))
+        return m
+    }
+}
+
