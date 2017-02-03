5191e0f5774ec9ef020876b89fd4912648f6d746
Nick Thomas <nichoth@gmail.com>
2017-02-03 14:24:09 -0800

    pass through values that arent streams

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var S = require('pull-stream/pull')
+var once = require('pull-stream/sources/once')
 S.drain = require('pull-stream/sinks/drain')
 var many = require('pull-many')
 
-module.exports = function flatMerge () {
+module.exports = function flatMerge (onErr) {
     return function sink (source) {
         var m = many()
         S(source, S.drain(function onEvent (s) {
+            if (typeof s !== 'function') {
+                return m.add(once(s))
+            }
             m.add(s)
         }, function onEnd (err) {
             m.cap()
+            if (err) {
+                if (onErr) onErr(err)
+                else throw err
+            }
         }))
         return m
     }
--- test/index.js
+++ test/index.js
@@ -2,6 +2,23 @@ var test = require('tape')
 var S = require('pull-stream')
 var flatMerge = require('../')
 
+test('pass through non streams', function (t) {
+    t.plan(2)
+    var source = S.values([
+        S.values([1,2]),
+        3
+    ])
+
+    S(
+        source,
+        flatMerge(),
+        S.collect(function (err, evs) {
+            t.error(err)
+            t.deepEqual(evs, [1,3,2], 'should pass through non streams')
+        })
+    )
+})
+
 test('flat merge', function (t) {
     t.plan(2)
     var ss = S.values([
