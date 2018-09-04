a265d9a7dc997720f161ca0e215ce87530b5aa67
nichoth <nichoth@gmail.com>
2018-09-04 01:07:33 -0700

    backwards compatible

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var S = require('pull-stream/pull')
+var once = require('pull-stream/sources/once')
 S.drain = require('pull-stream/sinks/drain')
 var many = require('pull-many')
 
@@ -6,6 +7,7 @@ module.exports = function flatMerge (onErr) {
     return function sink (source) {
         var m = many()
         S(source, S.drain(function onEvent (s) {
+            if (typeof s !== 'function') return m.add(once(s))
             m.add(s)
         }, function onEnd (err) {
             m.cap()
